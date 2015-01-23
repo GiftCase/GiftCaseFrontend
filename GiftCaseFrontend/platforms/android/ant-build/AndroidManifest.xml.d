e7411f2a3daddbd1fbe92ed33bcdcf2d73679953
AJLekc <alexandrasalikiriaki@gmail.com>
2015-01-23 01:07:48 +0100

    mmm
    mm

--- GiftCaseFrontend/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\AJLekc\University\MastersMalardalen\DistributedSystemsDevelopment\GiftCaseFrontend\GiftCaseFrontend\GiftCaseFrontend\platforms\android\ant-build\AndroidManifest.xml : \
-D:\AJLekc\University\MastersMalardalen\DistributedSystemsDevelopment\GiftCaseFrontend\GiftCaseFrontend\GiftCaseFrontend\platforms\android\AndroidManifest.xml \
